953f77575bdc1492ba85e93cad9b4f713addea40
Emre KÜÇÜK <emrekucuk74@hotmail.com>
2022-07-05 10:54:11 +0300

    Registry: Updated start_docker_registry.sh

--- registry/docker/start_docker_registry.sh
+++ registry/docker/start_docker_registry.sh
@@ -1,3 +1,7 @@
 docker run -d \
-    --name docker_registry \
-    registry:2
+  -p 5000:5000 \
+  --restart=always \
+  --name registry \
+  -v /var/docker_volumes/registry/docker:/var/lib/registry:rw \
+  -e REGISTRY_HTTP_HOST=https://registry.dev.crosstech.com.tr \
+  registry:2
