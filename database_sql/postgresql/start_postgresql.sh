2512eaac38c05a518a42e666fd75fbbb6655114a
Emin YILDIZ <eyeminyildiz@outlook.com>
2023-03-05 19:57:03 +0300

    PostgreSQL: Added Tags to Docker Images

--- database_sql/postgresql/start_postgresql.sh
+++ database_sql/postgresql/start_postgresql.sh
@@ -1,7 +1,8 @@
 docker run -d \
     --name postgresql \
     -p 5432:5432 \
+	--restart=always \
     -e POSTGRES_PASSWORD=postgres \
     -e PGDATA=/var/lib/postgresql/data/pgdata \
     -v /var/docker_volumes/databases/postgresql/data:/var/lib/postgresql/data \
-    postgres
+    postgres:14.6
--- database_sql/postgresql/start_postgresql_on_host_net.sh
+++ database_sql/postgresql/start_postgresql_on_host_net.sh
@@ -4,4 +4,4 @@ docker run -d \
     -e POSTGRES_PASSWORD=postgres \
     -e PGDATA=/var/lib/postgresql/data/pgdata \
     -v /var/docker_volumes/databases/postgresql/data:/var/lib/postgresql/data \
-    postgres
+    postgres:14.6
